c36717fb5a8dd50eb165f757c36b07a2a2908f9d
Mahek <mahek@datavoice.co.in>
2025-11-10 16:36:53 +0530

    ui fixed
    ui fixed

--- resources/views/attempt-questions.blade.php
+++ resources/views/attempt-questions.blade.php
@@ -153,6 +153,11 @@
             font-size: 18px;
             font-weight: bold;
         }
+        .user-welcome {
+            display: flex;
+            align-items: center;
+            gap: 10px;
+        }
     </style>
 </head>
 
@@ -162,14 +167,14 @@
         <!-- Left: Company Logo -->
         <div class="logo">
             {{-- <img src="{{ asset('images/logo.png') }}" alt="Company Logo"> --}}
-            <img src="{{ asset('images/logo2.png') }}" alt="Company Logo">
+            <img src="{{ asset('images/logo-2.png') }}" alt="Company Logo">
 
         </div>
 
         <!-- Right: User Info -->
         <div class="user-info">
-            <div>
-                <div style="font-size: 0.85rem;">Welcome back !</div>
+            <div class="user-welcome">
+                <div style="font-size:16px;">Welcome back !</div>
                 <div style="font-size: 24px; font-weight: bold;">{{ auth()->user()->name }}</div>
             </div>
             <a href="{{ route('logout') }}"
