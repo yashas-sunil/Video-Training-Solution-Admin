d1b97b11082802aacb15ea7f5617136a4dfd7a99
Bhagwan hake <bhagvanhake43@gmail.com>
2025-11-15 12:01:25 +0530

    bugs fix
    bugs fix

--- resources/views/emails/registration_mail.blade.php
+++ resources/views/emails/registration_mail.blade.php
@@ -17,10 +17,6 @@
             max-width: 660px;
         }
 
-        .header {
-            padding: 40px 30px 20px 30px;
-        }
-
         .innerpadding {
             padding: 30px 30px 30px 30px;
             line-height: 25px;
@@ -30,46 +26,10 @@
             border-bottom: 1px solid #f2eeed;
         }
 
-        .subhead {
-            font-size: 15px;
-            color: #ffffff;
-            letter-spacing: 10px;
-        }
-
-        .h1,
-        .h2,
-        .bodycopy {
-            color: #153643;
-        }
-
-        .h1 {
-            font-size: 33px;
-            line-height: 38px;
-            font-weight: bold;
-        }
-
-        .h2 {
-            padding: 0 0 15px 0;
-            font-size: 24px;
-            line-height: 28px;
-            font-weight: bold;
-        }
-
         .bodycopy {
             font-size: 16px;
             line-height: 22px;
-        }
-
-        .button {
-            text-align: center;
-            font-size: 18px;
-            font-weight: bold;
-            padding: 0 30px 0 30px;
-        }
-
-        .button a {
-            color: #ffffff;
-            text-decoration: none;
+            color: #153643;
         }
 
         .footer {
@@ -85,71 +45,42 @@
             color: #ffffff;
             text-decoration: underline;
         }
-
-        @media only screen and (max-width: 550px),
-        screen and (max-device-width: 550px) {
-            body[yahoo] .hide {
-                display: none !important;
-            }
-
-            body[yahoo] .buttonwrapper {
-                background-color: transparent !important;
-            }
-
-            body[yahoo] .button {
-                padding: 0px !important;
-            }
-
-            body[yahoo] .button a {
-                background-color: #effb41;
-                padding: 15px 15px 13px !important;
-            }
-
-            body[yahoo] .unsubscribe {
-                display: block;
-                margin-top: 20px;
-                padding: 10px 50px;
-                background: #2f3942;
-                border-radius: 5px;
-                text-decoration: none !important;
-                font-weight: bold;
-            }
-        }
     </style>
 </head>
 
 <body>
     <table bgcolor="#ffffff" class="content" align="center" cellpadding="0" cellspacing="0" border="0">
         <tbody>
+
+            <!-- Removed Header (orange area) -->
             <tr>
-                <td bgcolor="#f58457" class="header">
-                    <table width="70" align="center" border="0" cellpadding="0" cellspacing="0">
-                        <tbody>
-                            <tr>
-                                <td height="70" style="padding: 0 20px 20px 0;">
-                                    <!-- Logo ya image optional -->
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </td>
+                <td style="padding: 0;"></td>
             </tr>
+
             <tr>
                 <td class="innerpadding borderbottom" style="padding-top: 0px;">
+
                     Hi {{ $attributes['name'] }},
-                    <br /><br />
+                    <br><br>
+
                     Thank you for being a part of EduEdgeProLMS. Your EduEdgeProLMS account credentials are given below.
                     Do not share with others.
-                    <br /><br />
+                    <br><br>
+
                     <p><b>Email : {{ $attributes['email'] }}</b></p>
                     <p><b>Password : {{ $attributes['password'] }}</b></p>
-                    <br /><br />
+
+                    <br><br>
+
                     <a href="https://eduedgeprolms.datavoice.co.in/login"
-                        style="background-color: #f58457; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px;">Login
-                        Now</a>
-                    <br /><br />
+                        style="background-color: #f58457; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px;">
+                        Login Now
+                    </a>
+
+                    <br><br>
                 </td>
             </tr>
+
             <tr>
                 <td class="footer" bgcolor="#f58457">
                     <table width="100%" border="0" cellspacing="0" cellpadding="0">
@@ -163,6 +94,7 @@
                     </table>
                 </td>
             </tr>
+
         </tbody>
     </table>
 </body>
