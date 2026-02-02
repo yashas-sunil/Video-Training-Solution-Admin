e7ff4301e342d3199e6b331fa91e1c91cea6fb2d
Bhagvan Hake <bhagwan@datavoice.co.in>
2026-02-02 17:34:46 +0530

    Bugs fix
    Bugs fix

---
+++ database/migrations/2026_02_02_172002_create_authors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->id();
+             $table->string('name',88)->nullable();
+            $table->tinyInteger('status')->default(1)->comment('1-active, 0-inactive')->nullable();
+            $table->foreignId('created_by')->constrained('users')->nullable();
+            $table->foreignId('updated_by')->constrained('users')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors');
+    }
+}
