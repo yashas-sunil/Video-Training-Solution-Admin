4985b499d08b8f2f739b1b260063f9297764b74b
Bhagvan Hake <bhagwan@datavoice.co.in>
2026-03-25 17:45:11 +0530

    bugs fix
    bugs fix

--- resources/views/pages/admins/index.blade.php
+++ resources/views/pages/admins/index.blade.php
@@ -13,7 +13,7 @@
             <h1 class="m-0 text-dark">Users</h1>
         </div>
         <div class="col text-right">
-            <a href="{{ route('admins.create') }}" type="button" class="btn btn-success"><i class="fas fa-plus"></i> Create</a>
+            <a href="{{ route('admins.create') }}" type="button" class="btn btn-success"><i class="fas fa-plus"></i> Add User</a>
         </div>
     </div>
 @stop
