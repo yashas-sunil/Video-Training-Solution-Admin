a05d6066f8dcdcd6d7828d772c679268a47f8d12
Bhagvan Hake <bhagwan@datavoice.co.in>
2026-03-25 10:37:45 +0530

    pass min 8 ch
    pass min 8 ch

--- resources/views/pages/admins/create.blade.php
+++ resources/views/pages/admins/create.blade.php
@@ -114,8 +114,8 @@
                                                 name="mobile_code">
                                                 <option value="+91" {{ old('mobile_code') == '+91' ? 'selected' : '' }}>
                                                     +91</option>
-                                                <option value="+971"
-                                                    {{ old('mobile_code') == '+971' ? 'selected' : '' }}>+971</option>
+                                                {{-- <option value="+971"
+                                                    {{ old('mobile_code') == '+971' ? 'selected' : '' }}>+971</option> --}}
                                             </select>
                                             @error('mobile_code')
                                                 <span class="invalid-feedback d-block text-danger" role="alert">
