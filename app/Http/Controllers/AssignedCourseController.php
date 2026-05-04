99243994f35fd2c12b4e532d4f9c4290c49f3cfa
Bhagvan Hake <bhagwan@datavoice.co.in>
2026-05-04 18:32:35 +0530

    mail log table add
    mail log table add

--- app/Http/Controllers/AssignedCourseController.php
+++ app/Http/Controllers/AssignedCourseController.php
@@ -7,6 +7,7 @@ use App\CourseView;
 use App\EmailTemplate;
 use App\Mail\CourseAssignMail;
 use App\Models\Course;
+use App\Models\EmailLog;
 use App\Models\User;
 use App\ScormPackage as AppScormPackage;
 use App\ScormPackage;
@@ -119,9 +120,31 @@ class AssignedCourseController extends Controller
                 });
 
                 Log::info('Course Assign Mail SENT SUCCESS');
+
+                EmailLog::create([
+                    'user_id' => $user->id,
+                    'email'   => $user->email,
+                    'subject' => $template->subject,
+                    'body'    => $body,
+                    'status'  => 'sent',
+                    'cc'      => $template->cc,
+                    'bcc'     => $template->bcc,
+                ]);
             } catch (\Exception $e) {
+
                 Log::error('Course Assign Mail FAILED', [
                     'error' => $e->getMessage()
+                ]);
+
+                EmailLog::create([
+                    'user_id' => $user->id,
+                    'email'   => $user->email,
+                    'subject' => $template->subject,
+                    'body'    => $body,
+                    'status'  => 'failed',
+                    'error_message' => $e->getMessage(),
+                    'cc'      => $template->cc,
+                    'bcc'     => $template->bcc,
                 ]);
             }
         }
--- app/Models/EmailLog.php
+++ app/Models/EmailLog.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class EmailLog  extends Model
 {
     protected $table = 'email_logs';
+     protected $fillable = [
+        'user_id',
+        'email',
+        'subject',
+        'body',
+        'status',
+        'error_message',
+        'cc',
+        'bcc'
+    ];
 }
